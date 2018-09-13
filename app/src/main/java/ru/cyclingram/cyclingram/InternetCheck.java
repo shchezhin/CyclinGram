c58b4d956aa69a0db2be86fa208ae9edf9905ad3
Max Shchezhin <max.shchezhin@gmail.com>
2018-09-13 18:46:29 +0300

    Receiving stats (week of month)

--- app/src/main/java/ru/cyclingram/cyclingram/InternetCheck.java
+++ app/src/main/java/ru/cyclingram/cyclingram/InternetCheck.java
@@ -8,7 +8,8 @@ import java.net.Socket;
 
 public class InternetCheck extends AsyncTask<Void,Void,Boolean> {
 
-    @Override protected Boolean doInBackground(Void... voids) { try {
+    @Override protected Boolean doInBackground(Void... voids) {
+        try {
         Socket sock = new Socket();
         sock.connect(new InetSocketAddress("8.8.8.8", 53), 1500);
         sock.close();
--- app/src/main/java/ru/cyclingram/cyclingram/MainActivity.java
+++ app/src/main/java/ru/cyclingram/cyclingram/MainActivity.java
@@ -6,7 +6,12 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.text.DecimalFormat;
+import java.util.Calendar;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -30,6 +35,8 @@ public class MainActivity extends Activity {
 
         //checking the stored Strava API token. if no token, requesting it
         if(getStoredToken().length()!=40){authStrava();}
+
+        requestRides(getWeekStartTimestamp(), getCurrentTimestamp());
     }
 
     public void authStrava(){
@@ -81,13 +88,69 @@ public class MainActivity extends Activity {
         Call<RequestToken> requestToken(@Field("client_id") int client_id,
                                     @Field("client_secret") String client_secret,
                                     @Field("code") String code);
+
+        @GET("athlete/activities")
+        Call<List<Activity>> requestActivities(@Query("before") String before,
+                                               @Query("after") String after,
+                                               @Query("page") String page,
+                                               @Query("per_page") String per_page,
+                                               @Query("access_token") String access_token);
     }
 
-    public class RequestToken{
+    public class RequestToken {
         private int client_id;
         private String client_secret;
         private String code;
         private String access_token;
+    }
+
+    public class Activity {
+        private long id;
+        private String name;
+        private float distance;
+        private float moving_time;
+        private String type;
+        private String workout_type;
+        private boolean commute;
+    }
+
+    public void requestRides(int after, int before){
+        Retrofit statsRetrofit = new Retrofit.Builder()
+                .baseUrl("https://www.strava.com/api/v3/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        StravaApi activitiesStravaApi = statsRetrofit.create(StravaApi.class);
+        Call<List<Activity>> requestingActivities = activitiesStravaApi.requestActivities(
+                String.valueOf(before),String.valueOf(after), "1","90", getStoredToken());
+        Log.d(TAG, "is internet: " + internetCheck());
+        requestingActivities.enqueue(new Callback<List<Activity>>() {
+            @Override
+            public void onResponse(Call<List<Activity>> call, Response<List<Activity>> response) {
+                if (response.isSuccessful()) {
+                    doSmth(response.body());
+                }
+            }
+            @Override
+            public void onFailure(Call<List<Activity>> call, Throwable t) {
+                Log.d(TAG, "onFailure: " + t.getMessage());
+            }
+        });
+    }
+
+    public void doSmth (List<Activity> activities){
+        float commutes = 0, nonCommutes = 0;
+        for(int i=0; i<activities.size(); i++){
+            //Log.d(TAG, "Name: " + activities.get(i).name + " kms: " + activities.get(i).distance/1000 + " commute: " + activities.get(i).commute);
+            if(activities.get(i).commute) {commutes = commutes + activities.get(i).distance/1000;}
+            if(!activities.get(i).commute) {nonCommutes = nonCommutes + activities.get(i).distance/1000;}
+        }
+        Log.d(TAG, "commuted: " + commutes);
+        Log.d(TAG, "rides: " + nonCommutes);
+        Log.d(TAG, "total kms: " + (commutes+nonCommutes) );
+        TextView tvKms = findViewById(R.id.tv_kms);
+        float kms = commutes+nonCommutes;
+        String formattedKms = new DecimalFormat("#0.00").format(kms);
+        tvKms.setText(String.valueOf(formattedKms));
     }
 
     public boolean internetCheck(){
@@ -100,9 +163,9 @@ public class MainActivity extends Activity {
         }
         finally {
             if (inet){
-                Log.d(TAG, "internetCheck: internet OK");
+                //Log.d(TAG, "internetCheck: internet OK");
             } else {
-                Log.d(TAG, "internetCheck: NO internet");
+                //Log.d(TAG, "internetCheck: NO internet");
             }
             return inet;
         }
@@ -122,4 +185,44 @@ public class MainActivity extends Activity {
         String token = appSharedPrefs.getString("StravaToken","");
         return token;
     }
+
+    public int getWeekStartTimestamp(){ //getting the timestamp of the beginning of the week
+
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.HOUR_OF_DAY, 0);
+        cal.clear(Calendar.MINUTE);
+        cal.clear(Calendar.SECOND);
+        cal.clear(Calendar.MILLISECOND);
+        cal.clear(Calendar.AM_PM);
+
+        cal.setFirstDayOfWeek(Calendar.MONDAY);
+        cal.set(Calendar.DAY_OF_WEEK, cal.getFirstDayOfWeek());
+        Long timestamp = cal.getTimeInMillis() / 1000;
+        int inted = timestamp.intValue();
+
+        return inted;
+    }
+
+    public int getMonthStartTimestamp(){ //getting the timestamp of the beginning of the month
+
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.HOUR_OF_DAY, 0);
+        cal.clear(Calendar.MINUTE);
+        cal.clear(Calendar.SECOND);
+        cal.clear(Calendar.MILLISECOND);
+        cal.clear(Calendar.AM_PM);
+
+        cal.set(Calendar.DAY_OF_MONTH, 1);
+        Long timestamp = cal.getTimeInMillis() / 1000;
+        int inted = timestamp.intValue();
+
+        return inted;
+    }
+
+    public int getCurrentTimestamp(){
+        Long timestamp = System.currentTimeMillis()/1000;
+        int inted = timestamp.intValue();
+        return inted;
+    }
+
 }
