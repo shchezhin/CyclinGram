c22d0360330d09840ae89489480bb175a3b64d15
Max Shchezhin <max.shchezhin@gmail.com>
2018-09-18 16:54:10 +0300

    moved all auth to the auth activity with Strava button. + percentage output string

--- app/src/main/java/ru/cyclingram/cyclingram/MainActivity.java
+++ app/src/main/java/ru/cyclingram/cyclingram/MainActivity.java
@@ -9,14 +9,6 @@ import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 import java.util.Calendar;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
-import retrofit2.http.Field;
-import retrofit2.http.FormUrlEncoded;
-import retrofit2.http.POST;
 
 public class MainActivity extends Activity {
 
@@ -44,8 +36,17 @@ public class MainActivity extends Activity {
             String prevWeekAvg = myRequester.request(String.valueOf(Integer.parseInt(getWeekStartTimestamp()) - 604800), getWeekStartTimestamp(), getStoredToken());
             TextView thisWeekTV = findViewById(R.id.tv_kms);
             TextView prevWeekTV = findViewById(R.id.tv_kms2);
+            TextView txt = findViewById(R.id.textView3);
             thisWeekTV.setText(currWeekAvg);
             prevWeekTV.setText(prevWeekAvg);
+            if(Float.valueOf(currWeekAvg) >= Float.valueOf(prevWeekAvg)) {
+                String texxt = "This week I am " + String.format("%.1f", (100 * Float.valueOf(currWeekAvg) / Float.valueOf(prevWeekAvg) - 100)) + "% faster than previous week.";
+                txt.setText(texxt);
+            } else {
+                String texxt = "This week I am " + String.format("%.1f", (100 * Float.valueOf(prevWeekAvg) / Float.valueOf(currWeekAvg) - 100)) + "% slower than previous week.";
+                txt.setText(texxt);
+            }
+
     }
 
     public void authStrava(){
@@ -55,50 +56,15 @@ public class MainActivity extends Activity {
             Intent intent = new Intent(this, StravaLoginActivity.class);
             startActivityForResult(intent, 777);
         } else {
-            Toast toast = Toast.makeText(getApplicationContext(),
-                    "No internet connection",  Toast.LENGTH_SHORT);
-            toast.show(); //will handle logic here later
-            
+             //will handle logic here later
         }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        //getting the received code and requesting a token via Retrofit
-        if (data == null) {return;}
-        String code = data.getStringExtra("code");
-
-        Retrofit stravaRetrofit = new Retrofit.Builder()
-                .baseUrl("https://www.strava.com/oauth/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-        StravaApi tokenStravaApi = stravaRetrofit.create(StravaApi.class);
-
-        Call<RequestToken> requestingToken = tokenStravaApi.requestToken(25912,"fb921f750be783b4b8f4f44eaa80b7020c7aa657", code);
-        requestingToken.enqueue(new Callback<RequestToken>() {
-            @Override
-            public void onResponse(Call<RequestToken> call, Response<RequestToken> response) {
-                if (response.isSuccessful()) {
-                    if(response.body().access_token != null){
-                        saveToken(response.body().access_token);
-                        gettingStats();
-                    }
-                }
-            }
-            @Override
-            public void onFailure(Call<RequestToken> call, Throwable t) {
-                Log.d(TAG, "onFailure: " + t.getMessage());
-            }
-        });
-    }
-
-    public interface StravaApi {
-
-        @POST("token")
-        @FormUrlEncoded
-        Call<RequestToken> requestToken(@Field("client_id") int client_id,
-                                    @Field("client_secret") String client_secret,
-                                    @Field("code") String code);
+        if (requestCode == 777) {
+            gettingStats();
+        }
     }
 
     public boolean internetCheck() {
@@ -109,25 +75,18 @@ public class MainActivity extends Activity {
         } catch (Exception e) {
             Log.d(TAG, "authStrava: " + e.getMessage());
         }
-            if(!inet){Toast toast = Toast.makeText(getApplicationContext(),
+            if(!inet){
+                Toast toast = Toast.makeText(getApplicationContext(),
                     "No internet connection",  Toast.LENGTH_SHORT);
-                toast.show();}
+                toast.show();
+            }
             return inet;
         }
-
-    public void saveToken(String token){
-        SharedPreferences appSharedPrefs = PreferenceManager
-                .getDefaultSharedPreferences(this.getApplicationContext());
-        SharedPreferences.Editor prefsEditor = appSharedPrefs.edit();
-        prefsEditor.putString("StravaToken", token);
-        prefsEditor.commit();
-    }
 
     public String getStoredToken(){
         SharedPreferences appSharedPrefs = PreferenceManager
                 .getDefaultSharedPreferences(this.getApplicationContext());
-        String token = appSharedPrefs.getString("StravaToken","");
-        return token;
+        return appSharedPrefs.getString("StravaToken","");
     }
 
     public String getWeekStartTimestamp(){ //getting the timestamp of the beginning of the week
@@ -142,9 +101,8 @@ public class MainActivity extends Activity {
         cal.setFirstDayOfWeek(Calendar.MONDAY);
         cal.set(Calendar.DAY_OF_WEEK, cal.getFirstDayOfWeek());
         Long timestamp = cal.getTimeInMillis() / 1000;
-        String output = String.valueOf(timestamp);
 
-        return output;
+        return String.valueOf(timestamp);
     }
 
     public String getMonthStartTimestamp(){ //getting the timestamp of the beginning of the month
@@ -158,15 +116,13 @@ public class MainActivity extends Activity {
 
         cal.set(Calendar.DAY_OF_MONTH, 1);
         Long timestamp = cal.getTimeInMillis() / 1000;
-        String output = String.valueOf(timestamp);
 
-        return output;
+        return String.valueOf(timestamp);
     }
 
     public String getCurrentTimestamp(){
         Long timestamp = System.currentTimeMillis()/1000;
-        String output = String.valueOf(timestamp);
-        return output;
+        return String.valueOf(timestamp);
     }
 
 }
--- app/src/main/java/ru/cyclingram/cyclingram/StravaLoginActivity.java
+++ app/src/main/java/ru/cyclingram/cyclingram/StravaLoginActivity.java
@@ -1,24 +1,60 @@
 package ru.cyclingram.cyclingram;
 
 import android.app.Activity;
-import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import okhttp3.HttpUrl;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
 
 public class StravaLoginActivity extends Activity {
+
+    private static final String TAG = "mytag";
+    ImageButton stravaBtn;
+    TextView tv_btn;
+    ProgressBar pb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_strava_login);
 
+        tv_btn = findViewById(R.id.tv_btn);
+        pb = findViewById(R.id.progressBar);
+        stravaBtn = findViewById(R.id.stravaBtn);
+
+        stravaBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clicked();
+            }
+        });
+    }
+
+    private void clicked(){
+
+        pb.setVisibility(View.VISIBLE);
+        stravaBtn.setVisibility(View.INVISIBLE);
+        tv_btn.setVisibility(View.INVISIBLE);
+
         //going to Strava auth URL and catching the response code
 
-        HttpUrl authorizeUrl = HttpUrl.parse("https://www.strava.com/oauth/authorize") //
+        final HttpUrl authorizeUrl = HttpUrl.parse("https://www.strava.com/oauth/authorize") //
                 .newBuilder() //
                 .addQueryParameter("client_id", "25912")
                 .addQueryParameter("scope", "public")
@@ -36,20 +72,68 @@ public class StravaLoginActivity extends Activity {
                     Uri myUri = Uri.parse(url);
                     if(myUri.getQueryParameter("code")!=null) {
                         String code = myUri.getQueryParameter("code");
-                        Intent intent = new Intent();
-                        intent.putExtra("code", code);
-                        setResult(RESULT_OK, intent);
-                        finish();
-                    }
-                    else {
+                        auth(code);
+                    } else {
                         //later add some logic for if the code is null
                     }
                     return true;
                 }
                 else {return false;}
             }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                pb.setVisibility(View.INVISIBLE);
+            }
         });
 
         loginWebView.loadUrl(String.valueOf(authorizeUrl));
+    }
+
+    private void auth(String code) {
+
+        //getting the received code and requesting a token via Retrofit
+
+        Retrofit stravaRetrofit = new Retrofit.Builder()
+                .baseUrl("https://www.strava.com/oauth/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        StravaApi tokenStravaApi = stravaRetrofit.create(StravaApi.class);
+
+        Call<RequestToken> requestingToken = tokenStravaApi.requestToken(25912, "fb921f750be783b4b8f4f44eaa80b7020c7aa657", code);
+        requestingToken.enqueue(new Callback<RequestToken>() {
+            @Override
+            public void onResponse(Call<RequestToken> call, Response<RequestToken> response) {
+                if (response.isSuccessful()) {
+                    if (response.body().access_token != null) {
+                        saveToken(response.body().access_token);
+                        finish();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<RequestToken> call, Throwable t) {
+                Log.d(TAG, "onFailure: " + t.getMessage());
+            }
+        });
+    }
+
+    public interface StravaApi {
+
+        @POST("token")
+        @FormUrlEncoded
+        Call<RequestToken> requestToken(@Field("client_id") int client_id,
+                                        @Field("client_secret") String client_secret,
+                                        @Field("code") String code);
+    }
+
+    public void saveToken(String token){
+        SharedPreferences appSharedPrefs = PreferenceManager
+                .getDefaultSharedPreferences(this.getApplicationContext());
+        SharedPreferences.Editor prefsEditor = appSharedPrefs.edit();
+        prefsEditor.putString("StravaToken", token);
+        prefsEditor.commit();
     }
 }
